2170f43b89eee6e0c25cbdb1e060b05cb7369a46
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-01-31 08:32:29 +0700

    Add files via upload

---
+++ ms.sh
@@ -0,0 +1,42 @@
+#!/system/xbin/bash
+#Thanks To IDSYSTEM404
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo "\033[32;1m]======================================================="
+echo "\033[31;1m] ^...^  Spam Bom"
+echo "\033[31;1m]<_* *_> Coded by : InYourG00D"
+echo "\033[31;1m]  \_/   Team : LNX#CREW S.T.C BUFT"
+echo "\033[36;1m] kontak Me : 6281382406878"
+echo "\033[33;1m] Git Hub https://github.com/InYourG00D1"
+echo "\033[32;1m]======================================================="
+echo "MENU SPAM BOM" | lolcat
+echo $blue "1.>SPAM BOM BUKALAPAK"
+echo "[============================[>" | lolcat
+echo $cyan "2.>SPAM BOM JD-ID"
+echo "[============================[>" | lolcat
+echo $red "3.>SPAM BOM PHD"
+echo "[============================[>" | lolcat
+echo "4.>SPAM BOM TOKOPED"
+echo "[============================[>" | lolcat
+read -p "[root@InYourG00D:~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then 
+toilet "InYourG00D"
+cd 1
+php 1.php
+fi
+
+
+
+
+
+
+
+
