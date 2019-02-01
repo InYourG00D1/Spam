7fbf9a07928de302b754eee34acc44e145711fdf
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-02-01 14:41:27 +0700

    Add files via upload

---
+++ ms.sh
@@ -0,0 +1,99 @@
+#!/system/xbin/bash
+#Thanks To IDSYSTEM404
+#Jangan di recode dong ster 
+#usaha kalo gk bisa nanya 
+
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo "\033[32;1m]======================================================="
+echo "\033[31;1m] ^...^  Spam Bom"
+echo "\033[31;1m]<_* *_> Coded by : InYourG00D"
+echo "\033[31;1m]  \_/   Team : LNX#CREW S.T.C BUFT"
+echo "\033[36;1m] kontak Me : 6281382406878"
+echo "\033[33;1m] Git Hub https://github.com/InYourG00D1"
+echo "\033[32;1m]======================================================="
+echo $red "MENU SPAM BOM"
+echo $blue "1.>SPAM BOM BUKALAPAK"
+echo $yellow "[============================[>"
+echo $cyan "2.>SPAM BOM JD-ID"
+echo $green "[============================[>"
+echo $red "3.>SPAM BOM PHD"
+echo "[============================[>" | lolcat
+echo $yellow "4.>SPAM BOM TOKOPED"
+echo $blue  "[============================[>"
+echo $green "5.>Spam Bom Hooq"
+echo $red "[============================[>"
+echo $cyan "6.>Spam Bom KFC"
+echo $purple "[============================[>"
+echo $blue "7.>Spam Bom Matahari"
+echo $green "[============================[>"
+echo $cyan "00.> exit"
+echo $red "[============================[>"
+echo  $blue "Silahkan Pilih Jangan manja"
+read -p "[root@InYourG00D:~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then 
+toilet "InYourG00D" | lolcat
+cd spam
+php Bukalapak.php
+fi
+
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then 
+toilet "InYourG00D" | lolcat
+cd spam
+php error1.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then 
+toilet -f standard -F gay "InYourG00D"
+cd spam
+php PHD.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+toilet -f standard -F gay "InYourG00D"
+cd spam
+php TokoPed.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+toilet -f standard -F gay "InYourG00D"
+cd spam
+php error2.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+toilet -f standard -F gay "InYourG00D"
+cd spam
+php error3.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+toilet -f standard -F gay "InYourG00D"
+cd spam
+php error4.php
+fi
+
+if [ $bro = 00 ] || [ $bro =00 ]
+then 
+echo $red "Thank Bro Udah mampir"
+sleep 1
+echo $blue "lop yu"
+sleep 1
+exit
+fi
